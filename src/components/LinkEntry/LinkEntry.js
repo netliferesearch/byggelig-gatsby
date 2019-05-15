efc7e2a7813c302bd8bfa22211ae0ee56c217e3e
olemagnus <olestovern@gmail.com>
2019-05-15 11:10:22 +0200

    Adds linkentry component

---
+++ src/components/LinkEntry/LinkEntry.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Link } from 'gatsby';
+import PropTypes from 'prop-types';
+
+import './LinkEntry.scss';
+
+const LinkEntry = ({ children, to, icon }) => (
+  <Link to={to} className="link-entry">
+    {/* Should use the icon component here, above the text */}
+    {icon && <div>{icon}</div>}
+    {children}
+  </Link>
+);
+
+LinkEntry.propTypes = {
+  to: PropTypes.string.isRequired,
+  icon: PropTypes.string
+};
+
+export default LinkEntry;
--- src/pages/page-2.js
+++ src/pages/page-2.js
@@ -1,5 +1,7 @@
 import React from 'react';
-import { Link } from 'gatsby';
+
+import LinkBlock from '../components/LinkBlock';
+import LinkEntry from '../components/LinkEntry';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
@@ -8,8 +10,17 @@ const SecondPage = () => (
   <Layout>
     <SEO title="Page two" />
     <h1>Hi from the second page</h1>
-    <p>Welcome to page 2</p>
-    <Link to="/">Go back to the homepage</Link>
+    <p>Welcome to page 200000</p>
+    <LinkEntry to="/" icon="Some icon">
+      1: Planinitiativ
+    </LinkEntry>
+    <br />
+    <br />
+    <LinkBlock to="/">Go back to the homepage</LinkBlock>
+    <br />
+    <br />
+    <br />
+    <br />
   </Layout>
 );
 
