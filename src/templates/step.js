0bf9d66361a952e610ae94f9d9e746a364bbd8dc
tommyno <tommy@spindelvev.net>
2019-05-16 12:56:06 +0200

    Add role based query to Steps

--- src/templates/step.js
+++ src/templates/step.js
@@ -1,14 +1,39 @@
 import React from 'react';
-import { graphql } from 'gatsby';
+import { graphql, Link } from 'gatsby';
+import BlockContent from '@sanity/block-content-to-react';
 
 import Layout from '../components/layout';
 
-export default ({ data }) => {
+export default ({ data, pageContext }) => {
   console.log(data);
-  const { title = '' } = data.sanityStep;
+  const { role = '' } = pageContext;
+  const {
+    title = '',
+    intro = '',
+    stepNumber = '',
+    _rawMustHave: advices = {}
+  } = data.sanityStep;
+  console.log('advice', advices);
   return (
     <Layout>
-      <h1>Fase: {title}</h1>
+      <h1>
+        Fase {stepNumber}:<br />
+        {title}
+      </h1>
+      <p>{intro}</p>
+      <h2>Rolle: {role}</h2>
+      <h2>Dette må du ha på plass</h2>
+      <ul>
+        {advices
+          .filter(advice => advice.role.includes(role))
+          .map(advice => {
+            return (
+              <li>
+                <BlockContent blocks={advice.text} />
+              </li>
+            );
+          })}
+      </ul>
     </Layout>
   );
 };
@@ -18,6 +43,9 @@ export const query = graphql`
     sanityStep(id: { eq: $id }) {
       id
       title
+      intro
+      stepNumber
+      _rawMustHave
     }
   }
 `;
