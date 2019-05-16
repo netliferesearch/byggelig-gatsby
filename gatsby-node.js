a5ae121d9716169c122635d7327658efd7a0062a
tommyno <tommy@spindelvev.net>
2019-05-16 11:18:53 +0200

    Create two pages per step (one per role)

--- gatsby-node.js
+++ gatsby-node.js
@@ -29,19 +29,38 @@ async function createStepPages(graphql, actions, reporter) {
 
   postEdges.forEach((edge, index) => {
     const { id, stage, stepNumber, slug = {} } = edge.node;
-    const path = `/${stage}/steg${stepNumber}-${slug.current}/`;
-    reporter.info(`Creating step page: ${path}`);
+    const path = `/${stage}/steg${stepNumber}-${slug.current}`;
+    const pathUtbygger = `${path}/utbygger`;
+    const pathEntreprenor = `${path}/entreprenor`;
+    reporter.info(`Creating step page: ${pathUtbygger}`);
+    reporter.info(`Creating step page: ${pathEntreprenor}`);
 
+    // Create pages for utbygger
     createPage({
-      path,
+      path: pathUtbygger,
       component: require.resolve('./src/templates/step.js'),
-      context: { id }
+      context: { id, role: 'utbygger' }
     });
 
-    createPageDependency({ path, nodeId: id });
+    // Create pages for entreprenør
+    createPage({
+      path: pathEntreprenor,
+      component: require.resolve('./src/templates/step.js'),
+      context: { id, role: 'entreprenor' }
+    });
+
+    createPageDependency({ pathUtbygger, nodeId: id });
+    createPageDependency({ pathEntreprenor, nodeId: id });
   });
 }
 
 exports.createPages = async ({ graphql, actions, reporter }) => {
   await createStepPages(graphql, actions, reporter);
 };
+
+// /byggeprosess/steg1-behov/[entreprenor/utbygger]
+// context (id OG )
+
+// Generer to sett med sider
+// Hver side har en id context
+// Må bruke url for å vite "role" og matche mot advice
