a4293e4fd4a69aa99c8643a5f8e8e807b807d565
olemagnus <olestovern@gmail.com>
2019-05-20 21:23:51 +0200

    Adds new footer design

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -1,11 +1,29 @@
 import React from 'react';
+import ntnuLogo from '../../images/ntnu.svg';
 
 import './Footer.scss';
 
 export default () => (
   <footer className="footer">
     <div className="wrap-outer">
-      <div className="container-fluid">Hode, kne, footer og tær.</div>
+      <div className="container-fluid">
+        <div className="row">
+          <div className="col-md-3 mt-md-0 mt-2">
+            <img src={ntnuLogo} alt="NTNU Logo" />
+          </div>
+          <div className="col-md-5 mt-md-0 mt-2">
+            Veiledningen for effektive og bærekraftige byggeprosesser er
+            kvalitetssikret av NTNU
+          </div>
+          <div className="col-md-3 offset-md-1 mt-md-0 mt-2">
+            Lurer du på noe?
+            <br />
+            <a href="mailto:kontakt@byggelig.no" className="footer__link">
+              kontakt@byggelig.no
+            </a>
+          </div>
+        </div>
+      </div>
     </div>
   </footer>
 );
