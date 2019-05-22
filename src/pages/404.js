c8227c18f2c89b3ccce9bd070d17c96e36b02d36
tommyno <tommy@spindelvev.net>
2019-05-22 14:28:56 +0200

    Add some structure and content to 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -2,12 +2,26 @@ import React from 'react';
 
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import Breadcrumbs from '../components/Breadcrumbs';
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Siden finnes ikke" />
+    <Breadcrumbs />
+    <div class="wrap-outer">
+      <div class="container-fluid">
+        <div class="row">
+          <div class="col mt-4 mb-4">
+            <h1>Oida, denne siden finnes ikke...</h1>
+            <p>
+              Håper du ikke ble veldig skuffet. <br />
+              <br />
+              Ta en tur innom forsiden og se om du finner noe der.{' '}
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
   </Layout>
 );
 
