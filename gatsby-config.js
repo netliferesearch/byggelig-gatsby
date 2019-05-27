967ab283d5bded44eaf59cf7064ef5b3193f5a2e
tommyno <tommy@spindelvev.net>
2019-05-27 10:44:25 +0200

    Update manifest settings

--- gatsby-config.js
+++ gatsby-config.js
@@ -43,11 +43,11 @@ module.exports = {
     {
       resolve: `gatsby-plugin-manifest`,
       options: {
-        name: `gatsby-starter-default`,
-        short_name: `starter`,
+        name: `Bygg21`,
+        short_name: `bygg21`,
         start_url: `/`,
-        background_color: `#663399`,
-        theme_color: `#663399`,
+        background_color: `#2d0469`,
+        theme_color: `#2d0469`,
         display: `minimal-ui`,
         icon: `src/images/favicon-example.png` // This path is relative to the root of the site.
       }
