c65f3b586c1b61f739f806fdc0c87c0bf96a5f9c
olemagnus <olestovern@gmail.com>
2019-05-27 14:59:55 +0200

    Fixes instant scroll

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -8,3 +8,10 @@ export const onServiceWorkerUpdateReady = () => {
   // Refresh page if content has changed
   window.location.reload();
 };
+
+export const shouldUpdateScroll = ({ routerProps: { location } }) => {
+  if (location && location.state && location.state.noScroll) {
+    return false;
+  }
+  return true;
+};
