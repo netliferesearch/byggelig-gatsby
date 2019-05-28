da7a9408777d8863fd0f3a5acfc3df74402de952
tommyno <tommy@spindelvev.net>
2019-05-28 12:11:34 +0200

    Add better defaulting for role and localStorage

--- src/pages/index.js
+++ src/pages/index.js
@@ -26,7 +26,8 @@ export default ({ data }) => {
   } = data.sanityPage || {};
 
   // Check if a role is defined in localStorage, if not we default to the role "utbygger"
-  const defaultRole = localStorage.getItem('role') || 'utbygger';
+  const defaultRole =
+    localStorage.getItem('role') === 'entreprenor' ? 'entreprenor' : 'utbygger';
 
   // Toggle animation when items are in view
   const [isInViewList1, setIsInViewList1] = useState(false);
