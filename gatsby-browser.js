32da8eca64d96e9c4297e9f4870fab4cd26730ce
tommyno <tommy@spindelvev.net>
2019-05-29 11:09:57 +0200

    Add comment explaining the service worker

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,6 +4,7 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+// Used by "gatsby-offline-plugin"
 export const onServiceWorkerUpdateReady = () => {
   // Refresh page if content has changed
   window.location.reload();
