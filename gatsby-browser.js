41d2d0a5db2572ad85b91739b80e553f58169909
olemagnus <olestovern@gmail.com>
2019-06-03 14:41:57 +0200

    removes pwa for now

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,11 +4,12 @@
  * See: https://www.gatsbyjs.org/docs/browser-apis/
  */
 
+// Ole: lets wait with PWA until everything else is done
 // Used by "gatsby-offline-plugin"
-export const onServiceWorkerUpdateReady = () => {
-  // Refresh page if content has changed
-  window.location.reload();
-};
+// export const onServiceWorkerUpdateReady = () => {
+//   // Refresh page if content has changed
+//   window.location.reload();
+// };
 
 export const shouldUpdateScroll = ({ routerProps: { location } }) => {
   if (location && location.state && location.state.noScroll) {
