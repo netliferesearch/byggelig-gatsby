5bd1b4b8b685dd56afc2c137a73313695e574c32
olemagnus <olestovern@gmail.com>
2019-06-03 16:55:32 +0200

    Adjusts spacing for mobile

--- src/components/ArticlePitch/ArticlePitch.js
+++ src/components/ArticlePitch/ArticlePitch.js
@@ -17,12 +17,18 @@ const ArticlePitch = ({
     {subtle ? (
       <>
         <div className="article-pitch__text-content article-pitch__text-content--subtle">
-          <h2 className="article-pitch__title article-pitch__title--subtle">
-            {title}
-          </h2>
-          <p className="article-pitch__intro">{intro}</p>
-          <div className="article-pitch__link article-pitch__link--subtle">
-            <LinkBlock to={to}>{linkText}</LinkBlock>
+          <div className="container">
+            <div className="row">
+              <div className="col-12">
+                <h2 className="article-pitch__title article-pitch__title--subtle">
+                  {title}
+                </h2>
+                <p className="article-pitch__intro">{intro}</p>
+                <div className="article-pitch__link article-pitch__link--subtle">
+                  <LinkBlock to={to}>{linkText}</LinkBlock>
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </>
