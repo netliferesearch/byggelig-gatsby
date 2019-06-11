b038424e1cf0b395a3cc11f4485f123fe8269cbe
olemagnus <olestovern@gmail.com>
2019-06-11 18:34:10 +0200

    fixes footer placement

--- src/pages/404.js
+++ src/pages/404.js
@@ -4,25 +4,27 @@ import Layout from '../components/layout';
 import SEO from '../components/seo';
 
 const NotFoundPage = () => (
-  <Layout>
+  <>
     <SEO title="Siden finnes ikke" />
-    <div className="wrap-outer">
-      <div className="container-fluid">
-        <div className="row">
-          <div className="col mt-4 mb-4">
-            <h1>Denne siden finnes ikke...</h1>
-            <p>
-              Siden ble ikke prosjektert riktig og er derfor ikke bygget.
-              <br />
-              <br />
-              Gå til <a href="/">forsiden</a> og se om du kan finne tips til
-              byggeprosessen din der.{' '}
-            </p>
+    <Layout>
+      <main className="wrap-outer">
+        <div className="container-fluid">
+          <div className="row">
+            <div className="col mt-4 mb-4">
+              <h1>Denne siden finnes ikke...</h1>
+              <p>
+                Siden ble ikke prosjektert riktig og er derfor ikke bygget.
+                <br />
+                <br />
+                Gå til <a href="/">forsiden</a> og se om du kan finne tips til
+                byggeprosessen din der.
+              </p>
+            </div>
           </div>
         </div>
-      </div>
-    </div>
-  </Layout>
+      </main>
+    </Layout>
+  </>
 );
 
 export default NotFoundPage;
