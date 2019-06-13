530bddf7fdb64262aef597feb85ed1e2243e4539
olemagnus <olestovern@gmail.com>
2019-06-13 09:50:33 +0200

    Fixes realtime editing and removes unsued packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,7 @@
+require('dotenv').config({
+  path: `.env`
+});
+
 module.exports = {
   siteMetadata: {
     title: `Byggelig`,
@@ -10,9 +14,9 @@ module.exports = {
       options: {
         projectId: 'w6mawr56',
         dataset: 'production',
-        // a token with read permissions is required
-        // if you have a private dataset
-        token: process.env.MY_SANITY_TOKEN
+        token: process.env.SANITY_GATSBY_TOKEN,
+        watchMode: true,
+        overlayDrafts: true
       }
     },
     `gatsby-plugin-react-helmet`,
@@ -44,17 +48,7 @@ module.exports = {
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
     `gatsby-plugin-sass`,
-    {
-      resolve: `gatsby-plugin-purgecss`,
-      options: {
-        // printRejected: true // Print removed selectors and processed file names
-        // develop: true, // Enable while using `gatsby develop`
-        // tailwind: true, // Enable tailwindcss support
-        // whitelist: ['whitelist'], // Don't remove this selector
-        // ignore: ['/ignored.css', 'prismjs/', 'docsearch.js/'], // Ignore files/folders
-        // purgeOnly : ['components/', '/main.css', 'bootstrap/'], // Purge only these files/folders
-      }
-    },
+    `gatsby-plugin-purgecss`,
     `gatsby-plugin-sitemap`,
     {
       resolve: `gatsby-plugin-manifest`,
@@ -67,10 +61,6 @@ module.exports = {
         display: `minimal-ui`,
         icon: `src/images/favicon-example.png` // This path is relative to the root of the site.
       }
-    },
-    `gatsby-plugin-offline`
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    }
   ]
 };
