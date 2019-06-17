407e6d0c2d6a7344502ffb2905f6adb8fb07fd8b
olemagnus <olestovern@gmail.com>
2019-06-17 15:12:30 +0200

    removes header nav on frontpage

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { StaticQuery, graphql, Link } from 'gatsby';
 
@@ -6,24 +6,32 @@ import './Header.scss';
 
 const Header = ({ data }) => {
   const { siteName = '', mainSubHeading = '' } = data.sanitySettings;
+  const [hideHome, setHideHome] = useState(false);
+
+  useEffect(() => {
+    if (window.location.pathname === '/') setHideHome(true);
+  }, []);
+
   return (
     <header className="header">
       <div className="wrap-outer">
         <div className="container-fluid">
-          <div className="row">
-            <div className="col-4">
+          <div className="row no-gutters">
+            <div className="col">
               <Link to="/" className="header__title">
                 {siteName}
               </Link>
             </div>
-            <div className="col-4">
+            <div className="col">
               <nav className="header__nav">
-                <Link to="/" className="link link--white">
-                  Alle fasene
-                </Link>
+                {!hideHome && (
+                  <Link to="/" className="link link--white">
+                    Alle fasene
+                  </Link>
+                )}
               </nav>
             </div>
-            <div className="col-4 d-none d-md-block">
+            <div className="col d-none d-md-block">
               <div className="header__description">{mainSubHeading}</div>
             </div>
           </div>
