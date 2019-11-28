71f5c5c53a8cbb1d723aeac4e2ad74eb76caac2f
Torgeir Beyer <torgeir.beyer@netlife.com>
2019-11-28 14:51:32 +0100

    add dashboard netlify widget

--- studio/dashboardConfig.js
+++ studio/dashboardConfig.js
@@ -1,7 +1,37 @@
 export default {
   widgets: [
     {
-      name: 'project-info'
+      name: 'project-info',
+      options: {
+        __experimental_before: [
+          {
+            name: 'netlify',
+            options: {
+              title: 'My Netlify deploys',
+              sites: [
+                {
+                  title: 'Byggelig',
+                  apiId: '9c9c731f-0838-48e5-8380-4eb3137cd655',
+                  buildHookId: '5cd98ba747a849ca5fbcff05',
+                  name: 'byggelig',
+                }
+              ]
+            }
+          }
+        ],
+        data: [
+          {
+            title: 'GitHub repo',
+            value: 'https://github.com/netliferesearch/byggelig-gatsby',
+            category: 'Code'
+          },
+          {
+            title: 'Frontend',
+            value: 'https://bygglig.netlify.com',
+            category: 'apps'
+          }
+        ]
+      }
     },
     {
       name: 'project-users'
